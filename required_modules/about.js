892944f53266d9ddff0e7ac621bd0984b2eaf3c5
Lyyka <pwnzlps@gmail.com>
2022-12-15 19:56:05 -0500

    Code Updates
    Bringing the code inline with current dev branch

--- required_modules/about.js
+++ required_modules/about.js
@@ -1,7 +1,8 @@
 //Begin about module
 
 module.exports = {
-	about: function(client, channel){
+	about: function(client, channel, context){
+		console.log(`* ${context.username} used about.`)
 		client.say(channel, "Hi! My name is PupperishBot! I was built by my master LyykaPaws! (https://twitch.tv/lyykapaws) I operate using Node.JS! Eventually I'd like to become a bot used all cross twitch! But that's a big dream, and its a long way off.");
 	}
 }
